4195752c874fb3f4ecd7abc125078c8a310c5db4
liyujia <hb.liyujia@aisino.com>
2021-06-07 14:44:15 +0800

    feat:路由配置

--- src/route.js
+++ src/route.js
@@ -1,7 +1,7 @@
 import {createRouter,createWebHashHistory} from 'vue-router'
-
+// 1. 定义路由组件， 注意，这里一定要使用 文件的全名（包含文件后缀名）
 import Design from "./components/HelloWorld.vue";
-
+// 2. 定义路由配置
 const routes = [
   {
     path:'/',
@@ -11,10 +11,11 @@ const routes = [
     component:Design
   }
 ]
-
+// 3. 创建路由实例
 const router = createRouter({
+  // 4. 采用hash 模式
   history:createWebHashHistory(),
-  routes
+  routes//使用上方定义的路由配置
 })
 
 export default router
